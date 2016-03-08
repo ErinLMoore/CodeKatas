184660af6ee762075911ac4654f9eedcdd80fab9
Erin Moore <emoore@pillartechnology.com>
2016-03-08 14:02:31 -0500

    now returns fizz if given 3

--- rubykatas/fizzbuzz.rb
+++ rubykatas/fizzbuzz.rb
@@ -1,5 +1,9 @@
 class Fizzbuzz
   def fizzbuzzer(input_int)
-    return input_int
+    if input_int == 3
+      return "fizz"
+    else
+      return input_int
+    end
   end
 end
--- rubykatas/fizzbuzz_test.rb
+++ rubykatas/fizzbuzz_test.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'fizzbuzz'
+
 class FizzBuzzTest < Test::Unit::TestCase
     def test_return_one_if_given_one
     	fizzbuzz = Fizzbuzz.new
@@ -12,4 +13,11 @@ class FizzBuzzTest < Test::Unit::TestCase
     	expected = fizzbuzz.fizzbuzzer 2
     	assert_equal expected, 2
     end
+
+    def test_return_fizz_if_given_three
+    	fizzbuzz = Fizzbuzz.new
+    	expected = fizzbuzz.fizzbuzzer 3
+    	assert_equal expected, "fizz"
+    end
+
 end
