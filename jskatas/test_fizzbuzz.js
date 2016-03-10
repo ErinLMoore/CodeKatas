12c4029cc13a93370dc61e0309902dfa6e0699b9
Erin Moore <emoore@pillartechnology.com>
2016-03-10 10:54:50 -0500

    accidentally wrote code that passes first 2 tests at once.

--- jskatas/test_fizzbuzz.js
+++ jskatas/test_fizzbuzz.js
@@ -2,3 +2,8 @@ QUnit.test("returns 1 when given 1", function(assert) {
 	var result = fizzbuzzer(1);
 	assert.equal(result, 1);
 });
+
+QUnit.test("returns 2 when given 2", function(assert) {
+	var result = fizzbuzzer(2);
+	assert.equal(result, 2);
+});
