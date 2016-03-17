61322ca7cd6240e8b03c1c9a1e43c9962adb6b9f
Erin Moore <emoore@pillartechnology.com>
2016-03-17 15:07:27 -0400

    refactored to remove repititions

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -1,15 +1,16 @@
 import sys
 
 class ArabicToRoman(object):
+    def __init__(self):
+        self.roman_map = {'X':10, 'IX':9, 'V':5, 'IV':4}
 
     def return_value(self, input_int):
         return_val = ""
-        if input_int >= 5:
-            return_val += "V"
-            input_int -=5
-        elif input_int >= 4:
-            return_val += "IV"
-            input_int -=4
+        for key,val in self.roman_map.items():
+            if input_int >= val:
+                return_val += key
+                input_int -= val
+
         return_val += self.handle_ones(input_int)
         return return_val
 
