cb502d4b8b88a4ab8c3ce9a7a8dad39bd10c3ba0
Erin Moore <emoore@pillartechnology.com>
2016-03-17 14:16:53 -0400

    refactored ones into own method

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -5,7 +5,13 @@ class ArabicToRoman(object):
     def return_value(self, input_int):
         return_val = ""
         if input_int == 4:
-            return_val == "IV"
+            return_val = "IV"
+        else:
+            return_val = self.handle_ones(input_int)
+        return return_val
+
+    def handle_ones(self, input_int):
+        return_val = ""
         for i in range(input_int):
             return_val += "I"
         return return_val
