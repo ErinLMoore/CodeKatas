79bd35feb691f7da26b0692b1d17e4ab76cdf930
Erin Moore <emoore@pillartechnology.com>
2016-03-17 16:40:12 -0400

    refactored

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -6,26 +6,27 @@ class ArabicToRoman(object):
         self.arabic_roman_list= [("XL",40),("X",10), ("IX",9),("V", 5), ("IV",4)]
 
     def return_value(self, input_int):
-        return_val = ""
+        self.return_val = ""
         self.input_int = input_int
 
+        self.handle_repeatables()
+        self.handle_single_letters()
+        self.handle_ones()
+        return self.return_val
+
+    def handle_repeatables(self):
         for i in self.arabic_roman_multiples_list:
-            number_of_letters_to_add = input_int/i[1]
+            number_of_letters_to_add = self.input_int/i[1]
             if number_of_letters_to_add >=1:
-                return_val += i[0]*number_of_letters_to_add
+                self.return_val += i[0]*number_of_letters_to_add
                 self.input_int -= i[1]*number_of_letters_to_add
 
+    def handle_single_letters(self):
         for i in self.arabic_roman_list:
             if self.input_int >= i[1]:
-                return_val += i[0]
+                self.return_val += i[0]
                 self.input_int -= i[1]
 
-        return_val += self.handle_ones(self.input_int)
-        return return_val
-
-
-    def handle_ones(self, input_int):
-        return_val = ""
-        for i in range(input_int):
-            return_val += "I"
-        return return_val
+    def handle_ones(self):
+        for i in range(self.input_int):
+            self.return_val += "I"
