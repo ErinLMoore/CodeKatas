6a3d6c51f017ea8dbdb64748172158af2923475b
Erin Moore <emoore@pillartechnology.com>
2016-03-17 14:11:12 -0400

    works for 1-3

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -2,5 +2,8 @@ import sys
 
 class ArabicToRoman(object):
 
-    def return_value(self, input):
-        return 'I'
+    def return_value(self, input_int):
+        return_val = ""
+        for i in range(input_int):
+            return_val += "I"
+        return return_val
--- pythonkatas/romanumeral/romannumeraltest.py
+++ pythonkatas/romanumeral/romannumeraltest.py
@@ -6,5 +6,6 @@ class testRomanNumeral(unittest.TestCase):
     def setUp(self):
         self.atr = ArabicToRoman()
 
-    def test_returnsIifgiven1(self):
+    def test_returnsI_through_III_for_1_through_3(self):
         self.assertEqual('I', ArabicToRoman.return_value(self.atr, 1))
+        self.assertEqual('III', ArabicToRoman.return_value(self.atr, 3))
