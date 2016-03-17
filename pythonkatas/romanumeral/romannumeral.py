41132c823354b61521c7cb6bcf4c35a5e3d27194
Erin Moore <emoore@pillartechnology.com>
2016-03-17 15:37:15 -0400

    added test for 9. also refactoring happened

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -2,16 +2,17 @@ import sys
 
 class ArabicToRoman(object):
     def __init__(self):
-        self.roman_map = {'X':10, 'IX':9, 'V':5, 'IV':4}
+        self.arabic_roman_list= [("X",10), ("IX",9),("V", 5), ("IV",4)]
 
     def return_value(self, input_int):
         return_val = ""
-        for key,val in self.roman_map.items():
-            if input_int >= val:
-                return_val += key
-                input_int -= val
+        self.input_int = input_int
+        for i in self.arabic_roman_list:
+            if self.input_int >= i[1]:
+                return_val += i[0]
+                self.input_int -= i[1]
 
-        return_val += self.handle_ones(input_int)
+        return_val += self.handle_ones(self.input_int)
         return return_val
 
     def handle_ones(self, input_int):
