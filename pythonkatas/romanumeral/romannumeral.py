7320f2076d3609fd98ec13224cd7d9e916b3914a
Erin Moore <emoore@pillartechnology.com>
2016-03-18 15:12:14 -0400

    fixed it so it doesn't hate 70

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -2,30 +2,31 @@ import sys
 
 class ArabicToRoman(object):
     def __init__(self):
-        self.arabic_roman_multiples_list=[("C", 100), ("X",10)]
-        self.arabic_roman_list= [("XC", 90),("L",50),("XL",40),("IX",9),("V", 5), ("IV",4), ("I",1)]
+        self.multiples_list=['C','X']
+        self.arabic_roman_list= [("XC", 90),("L",50),("XL",40),("X",10),("IX",9),("V", 5), ("IV",4), ("I",1)]
         self.numerals_allowed_in_a_row= 3
 
     def return_value(self, input_int):
         self.return_val = ""
         self.input_int = input_int
-        self.handle_repeatables()
         self.handle_single_letters()
         self.handle_ones()
         return self.return_val
 
-    def handle_repeatables(self):
-        for letter_value_pair in self.arabic_roman_multiples_list:
-            number_of_letters_to_add = self.input_int/letter_value_pair[1]
-            if 1<= number_of_letters_to_add <= self.numerals_allowed_in_a_row:
-                self.return_val += letter_value_pair[0]*number_of_letters_to_add
-                self.input_int -= letter_value_pair[1]*number_of_letters_to_add
+    def handle_repeatables(self, letter_value_pair):
+        number_of_letters_to_add = self.input_int/letter_value_pair[1]
+        if 1<= number_of_letters_to_add <= self.numerals_allowed_in_a_row:
+            self.return_val += letter_value_pair[0]*number_of_letters_to_add
+            self.input_int -= letter_value_pair[1]*number_of_letters_to_add
 
     def handle_single_letters(self):
         for letter_value_pair in self.arabic_roman_list:
-            if self.input_int >= letter_value_pair[1]:
-                self.return_val += letter_value_pair[0]
-                self.input_int -= letter_value_pair[1]
+            if letter_value_pair[0] in self.multiples_list:
+                self.handle_repeatables(letter_value_pair)
+            else:
+                if self.input_int >= letter_value_pair[1]:
+                    self.return_val += letter_value_pair[0]
+                    self.input_int -= letter_value_pair[1]
 
     def handle_ones(self):
         for i in range(self.input_int):
