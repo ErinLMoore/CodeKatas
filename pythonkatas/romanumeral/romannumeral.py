b17dde41e00924a61ec8a82afb0932befa2d4ecd
Erin Moore <emoore@pillartechnology.com>
2016-03-18 13:21:31 -0400

    fixed bug for 40 and 400 but not so sure about 90

--- pythonkatas/romanumeral/romannumeral.py
+++ pythonkatas/romanumeral/romannumeral.py
@@ -3,7 +3,7 @@ import sys
 class ArabicToRoman(object):
     def __init__(self):
         self.arabic_roman_multiples_list=[("C", 100), ("X",10)]
-        self.arabic_roman_list= [("XC", 90),("L",50),("XL",40),("X",10), ("IX",9),("V", 5), ("IV",4)]
+        self.arabic_roman_list= [("XC", 90),("L",50),("XL",40),("IX",9),("V", 5), ("IV",4)]
 
     def return_value(self, input_int):
         self.return_val = ""
@@ -16,7 +16,7 @@ class ArabicToRoman(object):
     def handle_repeatables(self):
         for letter_value_pair in self.arabic_roman_multiples_list:
             number_of_letters_to_add = self.input_int/letter_value_pair[1]
-            if number_of_letters_to_add >=1:
+            if number_of_letters_to_add is not 4 and number_of_letters_to_add >=1:
                 self.return_val += letter_value_pair[0]*number_of_letters_to_add
                 self.input_int -= letter_value_pair[1]*number_of_letters_to_add
 
