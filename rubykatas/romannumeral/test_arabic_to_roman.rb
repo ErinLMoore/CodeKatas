b0bf7219741998ab21f92454e0c7f797b1350e25
Erin Moore <emoore@pillartechnology.com>
2016-03-22 16:32:53 -0400

    returns through IV

--- rubykatas/romannumeral/test_arabic_to_roman.rb
+++ rubykatas/romannumeral/test_arabic_to_roman.rb
@@ -2,15 +2,16 @@ require 'test/unit'
 require 'arabic_to_roman'
 
 class ArabicToRomanTest < Test::Unit::TestCase
-    def test_return_I_if_given_one
+    def test_return_I_through_III
     	atr = Arabic_To_Roman.new
     	expected = atr.return_value 1
     	assert_equal expected, 'I'
-    end
-
-    def test_return_III_if_given_three
-    	atr = Arabic_To_Roman.new
-    	expected = atr.return_value 3
+      expected = atr.return_value 3
     	assert_equal expected, 'III'
+    end
+    def test_return_IV_for_4
+    	atr = Arabic_To_Roman.new
+    	expected = atr.return_value 4
+    	assert_equal expected, 'IV'
     end
 end
