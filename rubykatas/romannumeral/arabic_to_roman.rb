aba443abbe6892da26da6a5a5b618632bc90cefc
Erin Moore <emoore@pillartechnology.com>
2016-03-22 16:32:53 -0400

    returns through IV

--- rubykatas/romannumeral/arabic_to_roman.rb
+++ rubykatas/romannumeral/arabic_to_roman.rb
@@ -2,8 +2,12 @@ class Arabic_To_Roman
 
   def return_value(input_int)
     return_value = ""
-    for i in 1..input_int
-      return_value += "I"
+    if input_int == 4
+      return_value = "IV"
+    else
+      for i in 1..input_int
+        return_value += "I"
+      end
     end
     return return_value
   end
