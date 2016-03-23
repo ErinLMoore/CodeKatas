a4f9258b40be9d85f4b81873a2e109f99c24ff97
Erin Moore <emoore@pillartechnology.com>
2016-03-23 13:28:12 -0400

    convinced ruby to return V for 5

--- rubykatas/romannumeral/test_arabic_to_roman.rb
+++ rubykatas/romannumeral/test_arabic_to_roman.rb
@@ -14,4 +14,9 @@ class ArabicToRomanTest < Test::Unit::TestCase
     	expected = atr.return_value 4
     	assert_equal expected, 'IV'
     end
+    def test_return_V_for_5
+    	atr = Arabic_To_Roman.new
+    	expected = atr.return_value 5
+    	assert_equal expected, 'V'
+    end
 end
