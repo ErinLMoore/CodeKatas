e22e5f03b66c698b7eefb820a6c2a9a4d1f91df2
Erin Moore <emoore@pillartechnology.com>
2016-03-28 19:00:47 -0400

    refactored tests

--- pythonkatas/romanumeral/testArabictoRoman.py
+++ pythonkatas/romanumeral/testArabictoRoman.py
@@ -6,9 +6,9 @@ class testArabictoRoman(unittest.TestCase):
     def setUp(self):
         self.atr = ArabicToRoman()
 
-    """def test_returns_for_multiples_of_1000(self):
+    def test_returns_for_multiples_of_1000(self):
         self.assertEqual('MM', ArabicToRoman.return_value(self.atr, 2000))
-        self.assertEqual('MMM', ArabicToRoman.return_value(self.atr, 3000))"""
+        self.assertEqual('MMM', ArabicToRoman.return_value(self.atr, 3000))
     def test_returns_for_multiples_of_100(self):
         self.assertEqual('CC', ArabicToRoman.return_value(self.atr, 200))
         self.assertEqual('CCC', ArabicToRoman.return_value(self.atr, 300))
