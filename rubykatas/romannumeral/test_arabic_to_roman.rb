638706e013240a3e6585e4cb4f1a34447fe4cfc7
Erin Moore <emoore@pillartechnology.com>
2016-04-08 15:44:37 -0400

    started over. passes 1st test.

--- rubykatas/romannumeral/test_arabic_to_roman.rb
+++ rubykatas/romannumeral/test_arabic_to_roman.rb
@@ -4,42 +4,8 @@ require 'arabic_to_roman'
 class ArabicToRomanTest < Test::Unit::TestCase
     def test_return_I_through_III
     	atr = Arabic_To_Roman.new
-    	expected = atr.return_value 1
-    	assert_equal expected, 'I'
-      expected = atr.return_value 3
-    	assert_equal expected, 'III'
-    end
-    def test_return_unique_singles
-    	atr = Arabic_To_Roman.new
-    	expected = atr.return_value 4
-    	assert_equal expected, 'IV'
-      expected = atr.return_value 5
-    	assert_equal expected, 'V'
-      expected = atr.return_value 9
-      assert_equal expected, 'IX'
+    	expected = atr.return_value 2000
+    	assert_equal expected, 'MM'
     end
 
-    def test_return_VI_through_VII
-      atr = Arabic_To_Roman.new
-      expected = atr.return_value 6
-      assert_equal expected, 'VI'
-      expected = atr.return_value 8
-      assert_equal expected, 'VIII'
-    end
-
-    def test_tens
-      atr = Arabic_To_Roman.new
-      expected = atr.return_value 10
-      assert_equal expected, 'X'
-      expected = atr.return_value 30
-      assert_equal expected, 'XXX'
-    end
-
-    def test_twenties_through_thirties
-      atr = Arabic_To_Roman.new
-      expected = atr.return_value 27
-      assert_equal expected, 'XXVII'
-      expected = atr.return_value 39
-      assert_equal expected, 'XXXIX'
-    end
 end
