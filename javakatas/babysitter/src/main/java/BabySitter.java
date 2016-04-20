dd1fe0ea587ab89e54429539daf3bb12511b217d
Erin Moore <emoore@pillartechnology.com>
2016-04-20 09:49:40 -0400

    tests if start time is 17 or after

--- javakatas/babysitter/src/main/java/BabySitter.java
+++ javakatas/babysitter/src/main/java/BabySitter.java
@@ -2,8 +2,10 @@ public class BabySitter {
   public static void main(String[] args){
     System.out.println("pretend this fizzbuzzes");
   }
-    public Boolean time_evaluator (int input){
-
-        return true;
+    public Boolean start_time_evaluator (int input){
+        if (input >=18)
+          {return true;}
+        else
+          {return false;}
     }
 }
--- javakatas/babysitter/src/test/java/BabySitterTest.java
+++ javakatas/babysitter/src/test/java/BabySitterTest.java
@@ -7,16 +7,24 @@ import org.junit.*;
 /**
  * Created by erinmoore on 3/15/16.
  */
-public class BabySitterTest{
-    BabySitter bs;
+ public class BabySitterTest{
+     BabySitter bs;
 
-    @Before
-    public void setUp(){
-         bs = new BabySitter();
-    }
+     @Before
+     public void setUp(){
+          bs = new BabySitter();
+     }
 
-    @Test
-    public void StartsNoEarlierThan5() {
-        assertEquals(true, bs.time_evaluator(6));
-    }
+     @Test
+     public void returns_true_if_start_time_17_or_after() {
+       Boolean expected = true;
+       Boolean result = bs.start_time_evaluator(18);
+       assertEquals(expected, result);
+     }
+     @Test
+     public void returns_false_if_start_time_before_17() {
+       Boolean expected = false;
+       Boolean result = bs.start_time_evaluator(15);
+       assertEquals(expected, result);
+     }
 }
