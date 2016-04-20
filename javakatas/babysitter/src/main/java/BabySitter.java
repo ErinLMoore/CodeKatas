e6142e9eadf2da53295ef5c136e56d16ba36184d
Erin Moore <emoore@pillartechnology.com>
2016-04-20 09:57:18 -0400

    returns true if end time before 28

--- javakatas/babysitter/src/main/java/BabySitter.java
+++ javakatas/babysitter/src/main/java/BabySitter.java
@@ -3,9 +3,17 @@ public class BabySitter {
     System.out.println("pretend this fizzbuzzes");
   }
     public Boolean start_time_evaluator (int input){
-        if (input >=18)
-          {return true;}
-        else
-          {return false;}
+      if (input >=18)
+        {return true;}
+      else
+        {return false;}
+    }
+    public Boolean end_time_evaluator (int input){
+      if (input<12)
+        {input += 12;}
+      if (input <=28)
+        {return true;}
+      else
+        {return false;}
     }
 }
