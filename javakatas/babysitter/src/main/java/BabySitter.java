1f6474ec27001f51a97654d1c733bdec914a5cb7
Erin Moore <emoore@pillartechnology.com>
2016-04-21 14:18:10 -0400

    passes salary tests

--- javakatas/babysitter/src/main/java/BabySitter.java
+++ javakatas/babysitter/src/main/java/BabySitter.java
@@ -2,15 +2,15 @@ public class BabySitter {
   public static void main(String[] args){
     System.out.println("babysitter main class");
   }
-  public Integer salary_evaluator (int start_time, int bed_time, int end_time){
-    Integer return_int = 0;
+  public int salary_evaluator (int start_time, int bed_time, int end_time){
+    int return_int = 0;
     if (end_time<12)
       {end_time += 24;}
 
     Boolean start_time_valid = start_time_evaluator(start_time);
     Boolean end_time_valid = end_time_evaluator(end_time);
 
-    if (start_time_valid == end_time_valid == true){
+    if (start_time_valid == true && end_time_valid == true){
       for (int i = start_time; i<end_time; i++){
         if (i < bed_time){
           return_int += 12;
@@ -20,7 +20,7 @@ public class BabySitter {
         }
         else{
           return_int += 16;
-        }
+          }
         }
       }
     return return_int;
@@ -32,7 +32,6 @@ public class BabySitter {
         {return false;}
     }
     public Boolean end_time_evaluator (int input){
-
       if (input <=28)
         {return true;}
       else
