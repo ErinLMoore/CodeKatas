d5621b7c8664797e698afbaec91d5ef07d7969dc
Erin Moore <emoore@pillartechnology.com>
2016-04-25 15:47:09 -0400

    made it wittier?

--- javakatas/babysitter/src/main/java/BabySitter.java
+++ javakatas/babysitter/src/main/java/BabySitter.java
@@ -1,50 +1,52 @@
+import java.util.stream.IntStream;
+
 public class BabySitter {
 
-  private int start_to_bed_wage = 12;
-  private int bed_to_mid_wage = 8;
-  private int mid_to_end_wage = 16;
+  //instead of for loop, use intersecional arrays or buckets or something i forget
 
-  public int check_time_and_evaluate_salary (int start_time, int bed_time, int end_time){
-    int total_salary = 0;
-    if (end_time<12)
-      {end_time += 24;}
+  private int startToBedWage = 12;
+  private int bedToMidWage = 8;
+  private int midToEndWage = 16;
+  private int minimumStartTime= 17;
+  private int maximumEndTime= 28;
+  private int midnight = 24;
 
-    Boolean start_time_valid = start_time_evaluator(start_time);
-    Boolean end_time_valid = end_time_evaluator(end_time);
+  public int checkTimeAndEvaluateSalary (int startTime, int bedTime, int endTime){
+    int totalSalary = 0;
+    endTime = normalizeEndTime(endTime);
 
-    if (start_time_valid && end_time_valid ){
-      total_salary = calculate_salary(start_time, bed_time, end_time);
+    Boolean startTimeValid = startTimeEvaluator(startTime);
+    Boolean endTimeValid = endTimeEvaluator(endTime);
+
+    if (startTimeValid && endTimeValid ){
+      totalSalary = calculateSalary(startTime, bedTime, endTime);
         }
-    return total_salary;
+    return totalSalary;
     }
 
-  public int calculate_salary(int start_time, int bed_time, int end_time){
-    int return_val = 0;
-    for (int i = start_time; i<end_time; i++){
-      if (i < bed_time){
-        return_val += start_to_bed_wage;
-      }
-      else if (i >= bed_time && i<24){
-        return_val += bed_to_mid_wage;
-      }
-      else{
-        return_val += mid_to_end_wage;
-        }
-      }
-      return return_val;
+  public int calculateSalary(int startTime, int bedTime, int endTime){
+  return IntStream.of((bedTime - startTime)*startToBedWage,
+                            (midnight - bedTime)*bedToMidWage,
+                            (endTime - midnight)*midToEndWage).sum();
     }
 
-  public Boolean start_time_evaluator (int start_time){
-    if (start_time >=17)
+  public Boolean startTimeEvaluator (int startTime){
+    if (startTime >=minimum_startTime)
       {return true;}
     else
       {return false;}
     }
-    
-  public Boolean end_time_evaluator (int end_time){
-    if (end_time <=28)
+
+  public Boolean endTimeEvaluator (int endTime){
+    if (endTime <=maximum_endTime)
       {return true;}
     else
       {return false;}
+  }
+
+  public int normalizeEndTime(int endTime){
+    if (endTime<midnight/2)
+      {endTime += midnight;}
+    return endTime;
   }
 }
