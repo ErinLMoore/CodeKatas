d4146f03ce5a7c6eff546c0c7c98a001115ce764
Erin Moore <emoore@pillartechnology.com>
2016-04-28 16:07:30 -0400

    added check for max promo length

--- jskatas/redpencil/test_redpencil.js
+++ jskatas/redpencil/test_redpencil.js
@@ -1,17 +1,17 @@
 QUnit.test("discount must be at least 5% to start promo", function(assert) {
-	var expected = true
+	var expected = true;
 	var result = discount_not_too_low(1, .5);
 	assert.equal(result, true);
-	var expected = false
+	var expected = false;
 	var result =  discount_not_too_low(1, .97);
 	assert.equal(result, false);
 });
 
 QUnit.test("discount must be %30 or under to start promo", function(assert) {
-	var expected = true
+	var expected = true;
 	var result = discount_not_too_high(1, .5);
 	assert.equal(result, true);
-	var expected = false
+	var expected = false;
 	var result = discount_not_too_high(1, .2);
 	assert.equal(result, false);
 });
@@ -20,10 +20,19 @@ QUnit.test("discount in right range", function(assert) {
 	var expected = true
 	var result = discount_in_right_range(1, .5);
 	assert.equal(result, true);
-	var expected = false
+	var expected = false;
 	var result = discount_in_right_range(1, .97);
 	assert.equal(result, false);
-	var expected = false
+	var expected = false;
 	var result = discount_in_right_range(1, .05);
+	assert.equal(result, false);
+});
+
+QUnit.test("promo length 30 days or less", function(assert) {
+	var expected = true
+	var result = promo_length_valid(15);
+	assert.equal(result, true);
+	var expected = false
+	var result = promo_length_valid(31);
 	assert.equal(result, false);
 });
