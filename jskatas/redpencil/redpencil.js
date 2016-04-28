30b7298ce29a31fd01165a77a71b17f0e14e0087
Erin Moore <emoore@pillartechnology.com>
2016-04-28 15:21:42 -0400

    one function tests both ranges

--- jskatas/redpencil/redpencil.js
+++ jskatas/redpencil/redpencil.js
@@ -1,3 +1,8 @@
+function discount_in_right_range(oldprice, newprice){
+  return discount_not_too_low(oldprice, newprice)
+     && discount_not_too_high(oldprice, newprice)
+}
+
 function  discount_not_too_low(oldprice, newprice) {
   var result = ((oldprice*.95)>=newprice);
   return result;
