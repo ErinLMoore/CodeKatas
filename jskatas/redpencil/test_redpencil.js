fd821a24570ad3e9d2da13bc34bb18a754f91c5e
Erin Moore <emoore@pillartechnology.com>
2016-04-28 15:21:42 -0400

    one function tests both ranges

--- jskatas/redpencil/test_redpencil.js
+++ jskatas/redpencil/test_redpencil.js
@@ -15,3 +15,15 @@ QUnit.test("discount must be %30 or under to start promo", function(assert) {
 	var result = discount_not_too_high(1, .2);
 	assert.equal(result, false);
 });
+
+QUnit.test("discount in right range", function(assert) {
+	var expected = true
+	var result = discount_in_right_range(1, .5);
+	assert.equal(result, true);
+	var expected = false
+	var result = discount_in_right_range(1, .97);
+	assert.equal(result, false);
+	var expected = false
+	var result = discount_in_right_range(1, .05);
+	assert.equal(result, false);
+});
