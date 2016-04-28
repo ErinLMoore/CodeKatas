991c3738fb601007f157d7da51ab3dc0dfbd5f77
Erin Moore <emoore@pillartechnology.com>
2016-04-28 16:07:30 -0400

    added check for max promo length

--- jskatas/redpencil/redpencil.js
+++ jskatas/redpencil/redpencil.js
@@ -1,14 +1,22 @@
+max_promo_length = 30
+min_discount_percent = .05
+max_discount_percent = .3
+
 function discount_in_right_range(oldprice, newprice){
-  return discount_not_too_low(oldprice, newprice)
-     && discount_not_too_high(oldprice, newprice)
+  return discount_not_too_low(oldprice, newprice)&&
+          discount_not_too_high(oldprice, newprice);
+}
+
+function promo_length_valid(promolength){
+  return (promolength <=max_promo_length);
 }
 
 function  discount_not_too_low(oldprice, newprice) {
-  var result = ((oldprice*.95)>=newprice);
+  var result = ((oldprice-newprice )>=oldprice*min_discount_percent);
   return result;
 }
 
 function discount_not_too_high(oldprice, newprice){
-  var result = ((oldprice*.30)<=newprice);
+  var result = ((oldprice*max_discount_percent)<=newprice);
   return result;
 }
