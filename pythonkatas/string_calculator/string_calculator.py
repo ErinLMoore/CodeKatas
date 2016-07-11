c50f15cae4e0b423b1deca45ce57d66027e54a6f
Erin Moore <emoore@pillartechnology.com>
2016-07-11 11:16:47 -0400

    reduced to oneline

--- pythonkatas/string_calculator/string_calculator.py
+++ pythonkatas/string_calculator/string_calculator.py
@@ -1,9 +1,5 @@
 def add(argument_string):
-    string_list = argument_string.split(",")
-    if len(string_list)==1:
-        if argument_string == "":
-            return "0"
-        else:
-            return argument_string
-    else:
-        return  str(int(string_list[0]) + int(string_list[1]))
+
+    if(argument_string == ""):
+        return "0"
+    return str(sum([int(i) for i in argument_string.split(",")]))
